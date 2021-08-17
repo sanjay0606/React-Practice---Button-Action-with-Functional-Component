a0caeb1c295ea9a0cb1ff2bf6351d50eee99ffe7
sanjay <sanjaysighania7@gmail.com>
2021-08-17 12:44:46 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [pvis,setpvis]=useState(true);
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={()=>setpvis(false)}>show para</button>
+      { (pvis) ? null: <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p> }
+
+
     </div>
   );
 }
